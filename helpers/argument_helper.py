e180feef9806b4582f7aef34b4cc3dd96d7a2a89
William Christian <wllchrst@outlook.com>
2025-10-15 08:38:20 +0700

    feat: parse using testing

---
+++ helpers/argument_helper.py
@@ -0,0 +1,10 @@
+﻿import argparse
+
+
+class ArgumentHelper:
+    @staticmethod
+    def parse_main_script():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--testing", help="It will not use the full dataset, just to make sure everything is going all right",
+                            action='store_true')
+        return parser.parse_args()
--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+from pipeline.lang_identification_pipeline import LangIdentificationPipeline
+from helpers.argument_helper import ArgumentHelper
 
 def main():
-    pass
+    arguments = ArgumentHelper.parse_main_script()
+    LangIdentificationPipeline(testing=arguments.testing)
 
 if __name__ == "__main__":
     main()
